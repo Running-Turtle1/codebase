a981214d74066eb80440dcf5197fd6e86f2b188d
Wei Pengchao(Running-Turtle1) <wei954327649@gmail.com>
2026-03-18 14:09:07 +0000

    1

--- imageGeneration/VAE/train_mnist.py
+++ imageGeneration/VAE/train_mnist.py
@@ -15,9 +15,11 @@ train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('./data', train=True, download=True, transform=transforms.ToTensor()),
     batch_size=batch_size,
     shuffle=True,
+    num_workers=4,
+    pin_memory=True
 )
 
-# 3. trian
+# 3. train
 model = VAE().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
@@ -28,16 +30,17 @@ def train(epoch):
     for batch_idx, (data, _) in enumerate(train_loader):
         data = data.to(device)
         optimizer.zero_grad()
-        recon_batch, mu, logvar = model(data)
+        recon_batch, mu, logvar = model(data.view(-1, 784))
         loss = loss_function(recon_batch, data, mu, logvar)
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
-    print(f'====> Epoch: {epochs} Average loss: {train_loss / len(train_loader.dataset):.4f}')
+    print(f'====> Epoch: {epoch} Average loss: {train_loss / len(train_loader.dataset):.4f}')
 
 
 # 5. 测试
 def test_generation(epoch):
+    model.eval()
     with torch.no_grad():
         # 随机从标准正态分布采样 64 个 z
         z = torch.randn(64, z_dim).to(device)
